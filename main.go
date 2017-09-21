414c8c2757f34d6a39746f388247de31452f3968
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-21 22:15:58 +0530

    set up the structs for request and response

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"io"
 	"os"
 )
+
+// VatQuery struct to store data to be sent as the request
+type VatQuery struct {
+	XMLName xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
+	Header  string   `xml:"Header"`
+	Body    struct {
+		CheckVat struct {
+			XMLName xml.Name `xml:"urn:ec.europa.eu:taxud:vies:services:checkVat:types checkVat"`
+			Country string   `xml:"countryCode"`
+			Vat     string   `xml:"vatNumber"`
+		}
+	} `xml:"Body"`
+}
+
+// VatResponse struct to store the data that we get as response
+type VatResponse struct {
+	XMLName xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
+	Body    struct {
+		CheckVatResponse struct {
+			XMLName     xml.Name `xml:"urn:ec.europa.eu:taxud:vies:services:checkVat:types checkVatResponse"`
+			Country     string   `xml:"countryCode"`
+			Vat         string   `xml:"vatNumber"`
+			RequestDate string   `xml:"requestDate"`
+			Valid       string   `xml:"valid"`
+			Name        string   `xml:"name"`
+			Address     string   `xml:"address"`
+		}
+	} `xml:"Body"`
+}
 
 func main() {
 	flag.Parse()
